f6d3df4d75e2d3a17f6b771b1a2bfeab9b43858d
ortlof <sm@mail4u2.de>
2022-08-13 00:39:43 +0200

    Fixed Pinout for V1 PCB's

--- src/config.h
+++ src/config.h
@@ -12,26 +12,31 @@ uint8_t CUM_Address[] ={0x34, 0xb4, 0x72, 0x12, 0xab, 0xa0};
 #define M5_ID 99 //M5_ID Default can be changed with M5 Remote in the Future will be Saved in EPROOM
 
 
+
+
+#define V2  //Define V1 or V2 PCB Version
+
 //PIn Definitons Encoders V1:
+#ifdef V1
+#define ENC_1_CLK 25
+#define ENC_1_DT 26
 
-//#define ENC_1_CLK 25
-//#define ENC_1_DT 26
+#define ENC_2_CLK 13
+#define ENC_2_DT 14
 
-//#define ENC_2_CLK 13
-//#define ENC_2_DT 14
+#define ENC_3_CLK 33
+#define ENC_3_DT 32
 
-//#define ENC_3_CLK 33
-//#define ENC_3_DT 32
-
-//#define ENC_4_CLK 19
-//#define ENC_4_DT 27
-//OneButton Button1(35, false);
-//OneButton Button2(38, false);
-//OneButton Button3(18, false);
-//#define ANALOGIN 36 
+#define ENC_4_CLK 19
+#define ENC_4_DT 27
+OneButton Button1(35, false);
+OneButton Button2(18, false);
+OneButton Button3(38, false);
+#endif
 
 //PIn Definitons Encoders V2:
 
+#ifdef V2
 #define ENC_1_CLK 25
 #define ENC_1_DT 26
 
@@ -46,6 +51,7 @@ uint8_t CUM_Address[] ={0x34, 0xb4, 0x72, 0x12, 0xab, 0xa0};
 OneButton Button1(35, false); //MX Button
 OneButton Button2(36, false); //Encoder Left
 OneButton Button3(34, false, true); // Encoder Right
+#endif
 
 #define Encoder_MAP 48
 
